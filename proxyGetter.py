184eb1b2f120887f3edc65f515fdf746ea364cdd
SoPudge <abcd666@gmail.com>
2017-03-29 10:43:36 +0800

    添加header，完成抓取部分

--- proxyGetter.py
+++ proxyGetter.py
@@ -1,28 +1,42 @@
 # -*- coding: utf-8 -*- 
 from urllib import request
+from html.parser import HTMLParser
+from bs4 import BeautifulSoup
+import re
 class getProxyPage():
     """docstring for getProxyPage"""
     """本类用于抓取代理服务器网页"""
     """包含多个代理服务器的网页"""
     def __init__(self):
         super(getProxyPage, self).__init__()
+        #建立haders
+        self.url = 'http://www.xicidaili.com/nt/'
+        self.req = request.Request(self.url)
+        self.req.add_header('Host','www.xicidaili.com')
+        self.req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0.1 Waterfox/51.0.1')
+        #self.req.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+        #建立存储字典
+        self.proxyStorage = {}
+
     def getHttp(self):
         #proxies = {'http': 'http://10.166.1.37:8080/'}
         #opener = request.FancyURLopener(proxies)
-        #proxy_support = request.ProxyHandler({'http':'10.166.1.37:8080'})
-        #opener = request.build_opener(proxy_support)
-        #request.install_opener(opener)
-        url = 'http://cn-proxy.com/'
-        with request.urlopen(url) as f:
-            print(f.status,f.reason)
-            for k,v in f.getheaders():
-                print(k,v)
+        proxy_support = request.ProxyHandler({'http':'10.166.1.37:8080'})
+        opener = request.build_opener(proxy_support)
+        request.install_opener(opener)
+        #url = 'http://www.xicidaili.com/nt/'
+        with opener.open(self.req) as f:
+            data = f.read().decode('utf-8')
+        return data
+
+    def decodePage(self,data):
+        soup = BeautifulSoup(data,'html.parser')
+        print(soup.find(re.compile(r'')))
 
 
-
-
-proxy = getProxyPage()
-proxy.getHttp()
-if __name__ == '__main__':
-    proxy = getProxyPage()
-    proxy.getHttp()
+proxypage = getProxyPage()
+proxydecode = proxypage.getHttp()
+proxypage.decodePage(proxydecode)
+#if __name__ == '__main__':
+#    proxy = getProxyPage()
+#    proxy.getHttp()
