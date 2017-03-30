e3b44f4ff562680d5daf24402b9cd9685dc272e3
SoPudge <abcd666@gmail.com>
2017-03-30 20:28:06 +0800

    完成检查代理方法，并重构变量类型和调用方式

--- proxyGetter.py
+++ proxyGetter.py
@@ -5,34 +5,37 @@ from bs4 import BeautifulSoup
 from collections import OrderedDict
 import re
 import time
-class getProxyPage(object):
+class getProxy(object):
     """docstring for getProxyPage"""
     """本类用于抓取代理服务器网页"""
     """包含多个代理服务器的网页"""
     def __init__(self):
-        super(getProxyPage, self).__init__()
+        super(getProxy, self).__init__()
         #建立haders
-        self.url = 'http://www.xicidaili.com/nt/'
-        self.req = request.Request(self.url)
-        self.req.add_header('Host','www.xicidaili.com')
-        self.req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0.1 Waterfox/51.0.1')
-        #建立存储字典
-        self.proxyStorage = {}
+        self._url = 'http://www.xicidaili.com/wt/'
+        self._req = request.Request(self._url)
+        self._req.add_header('Host','www.xicidaili.com')
+        self._req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0.1 Waterfox/51.0.1')
+        #全局变量，存储抓取页面，解析结果
+        self._page = ''
+        self._proxyStorage = {}
 
     def getHttp(self):
         #使用代理获取地址
-        proxy_support = request.ProxyHandler({'http':'10.166.1.37:8080'})
-        opener = request.build_opener(proxy_support)
-        request.install_opener(opener)
+        #proxy_support = request.ProxyHandler({'http':'10.166.1.37:8080'})
+        #opener = request.build_opener(proxy_support)
+        #request.install_opener(opener)
         #url = 'http://www.xicidaili.com/nt/'
         #读取对应网页
-        with opener.open(self.req) as f:
-            data = f.read().decode('utf-8')
-        return data
+        with request.urlopen(self._req) as f:
+            self._page = f.read().decode('utf-8')
+        #return self.page
 
-    def decodePage(self,data):
+    def decodePage(self):
         #本方法专门解析网页，获取详细Ip信息
-        soup = BeautifulSoup(data,'html.parser')
+        #此处先内部执行getHttp抓取网页
+        self.getHttp()
+        soup = BeautifulSoup(self._page,'html.parser')
         for i in range(1,101):
             soupFirst = soup.find_all('tr')[i]
             ipaddr = soupFirst.find_all('td')[1].string
@@ -42,13 +45,13 @@ class getProxyPage(object):
             else:
                 zone = soupFirst.find('a').string
             isTranspar = soupFirst.find_all(class_="country")[1].string
+            ishttp = soupFirst.find_all('td')[5].string
             linkspeed = soupFirst.find_all(class_="bar")[0]['title']
             linktime = soupFirst.find_all(class_="bar")[1]['title']
             alivetime = soupFirst.find_all('td')[-2].string
             updatetime = soupFirst.find_all('td')[-1].string
-            self.proxyStorage['%s:%s' % (ipaddr,port)] = {zone,isTranspar,linkspeed,linktime,alivetime,updatetime}
-            print(i,ipaddr,port,zone,isTranspar,linkspeed,linktime,alivetime,updatetime)
-        print(self.proxyStorage)
+            self._proxyStorage['%s:%s' % (ipaddr,port)] = {'zone':zone,'isTranspar':isTranspar,'ishttp':ishttp,'linkspeed':linkspeed,'linktime':linktime,'alivetime':alivetime,'updatetime':updatetime}
+        return self._proxyStorage
 
 
 class checkAlive(object):
@@ -57,28 +60,24 @@ class checkAlive(object):
     '''
     def __init__(self):
         pass
-    def isAlive(self): 
-        proxy_support = request.ProxyHandler({'http':'10.166.1.37:8080'})
+    def isAlive(self,ishttp,ipport): 
+        proxy_support = request.ProxyHandler({ishttp:ipport})
         opener = request.build_opener(proxy_support)
         request.install_opener(opener)
-        #读取对应网页
-        with opener.open('http://www.baidu.com') as f:
-            t1 = time.time()
+        #读取对应网页用以测试速度
+        t1 = time.time()
+        with opener.open('http://m.51job.com') as f:
             f.read().decode('utf-8')
-            t2 = time.time()
-        print(t2 - t1)
-
-
+        t2 = time.time()
+        return ('%s %0.4f ms' % (ipport,(t2 - t1)*1000))
 
 
 #class storeToSql(object)
 #本类存储到sql当中
 
-proxypage = getProxyPage()
-proxydecode = proxypage.getHttp()
-proxypage.decodePage(proxydecode)
-checkIP = checkAlive()
-checkIP.isAlive()
-#if __name__ == '__main__':
-#    proxy = getProxyPage()
-#    proxy.getHttp()
+proxylist = getProxy()
+proxyresult = proxylist.decodePage()
+checkresult = checkAlive()
+print(proxyresult.keys())
+for k,v in proxyresult.items():
+    print(checkresult.isAlive(v['ishttp'],k))
