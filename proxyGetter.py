689af2ae7d37c9e7e14d1e9b59b0afccebc6fb3b
root <root@debian>
2017-05-01 22:55:35 +0800

    修改获取代理逻辑，将多个网站获取代理逻辑放在一个类当中

--- proxyGetter.py
+++ proxyGetter.py
@@ -12,30 +12,23 @@ class getProxy(object):
     """包含多个代理服务器的网页"""
     def __init__(self):
         super(getProxy, self).__init__()
-        #建立haders
-        self._url = 'http://www.xicidaili.com/wn/'
-        self._req = request.Request(self._url)
-        self._req.add_header('Host','www.xicidaili.com')
-        self._req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0.1 Waterfox/51.0.1')
+        #建立headers
+        self._xiciheaders = {'Host':'www.xicidaili.com','User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0.1 Waterfox/51.0.1'}
         #全局变量，存储抓取页面，解析结果
-        self._page = ''
         self._proxyStorage = {}
 
-    def getHttp(self):
+    def getxici(self):
         #使用代理获取地址
         #proxy_support = request.ProxyHandler({'http':'10.166.1.37:8080'})
         #opener = request.build_opener(proxy_support)
         #request.install_opener(opener)
         #读取对应网页
-        with request.urlopen(self._req) as f:
-            self._page = f.read().decode('utf-8')
-        #return self.page
+        url = 'http://www.xicidaili.com/wn/'
+        req = request.Request(url,headers=self._xiciheaders)
+        with request.urlopen(req) as f:
+            page = f.read().decode('utf-8')
 
-    def decodePage(self):
-        #本方法专门解析网页，获取详细Ip信息
-        #此处先内部执行getHttp抓取网页
-        self.getHttp()
-        soup = BeautifulSoup(self._page,'html.parser')
+        soup = BeautifulSoup(page,'html.parser')
         soupFirst = soup.find_all('tr')
         templist = []
         #此处通过stripped_strings方法获取不带空格的，所有html标签的text，为一个generator
@@ -58,61 +51,6 @@ class getProxy(object):
         #此处重置templist，否则每次添加内容到同一个templist当中，导致字典只能获取第一个代理的值
             templist = []
         return self._proxyStorage
-
-class checkAlive(object):
-    '''本类专门用于检测代理的可用性
-       原理是使用代理访问baidu，看延迟
-       存储到专门的数据库，ipaddr,site,lag,statu,updatetime
-    '''
-    def __init__(self):
-        self._url = 'http://m.51job.com'
-        self._urlconfirm = '工作'
-        self._sqlname = 'proxy.db'
-        self._conn = sqlite3.connect(self._sqlname)
-        self._c = self._conn.cursor()
-        self._c.execute('CREATE TABLE IF NOT EXISTS status (ipaddr TEXT,site TEXT,lag TEXT,statu TEXT,updatetime TEXT)')
-        self._c.execute('SELECT ishttp,ipaddr,port FROM proxy')
-        self._httpipport = self._c.fetchall()
-        #将http ipaddr port 结果放在list当中，以便调用，这样不会重复读取数据库
-        self._c.execute('SELECT COUNT(ipaddr) FROM proxy')
-        self._proxynum = self._c.fetchall()[0][0]
-        #这里获取一共有多少条代理需要验证
-        
-    def isAlive(self):
-        num = 1
-        insertvalue = []
-        try:
-            for row in self._httpipport:
-                ishttp = row[0]
-                ipport = ('%s:%s' % (row[1],row[2]))
-                #循环读取数据库当中的ishttp和ipport字段
-                proxy_support = request.ProxyHandler({ishttp:ipport})
-                opener = request.build_opener(proxy_support)
-                request.install_opener(opener)
-                #读取对应网页用以测试速度
-                print('%s/%s 正在验证代理：%s:%s ...' % (num,self._proxynum,row[1],row[2]))
-                t1 = time.time()
-                with opener.open(self._url) as f:
-                    if self._urlconfirm in f.info():
-                        sitestatu = 'OK'
-                    else: sitestatu = 'NOK'
-                    t2 = time.time()
-                lagtime =('%0.2f' % ((t2-t1)*1000))
-                insertvalue.append((row[1],self._url,lagtime,sitestatu,'2017'))
-                #这里将需要写入数据库的内容先放在list当中，方便后续一次性execuatemany写入
-                print('http代码为 %s，延迟 %s ms' % (sitestatu,lagtime))
-                num = num + 1
-
-        finally:
-            self._c.executemany('INSERT INTO status VALUES (?,?,?,?,?)',insertvalue)
-            self._c.execute('SELECT COUNT(ipaddr) FROM status')
-            result = self._c.fetchall()[0][0]
-            print('合计验证了 %s 条结果' % result)
-            #查询写入多少条结果
-            self._c.close()
-            self._conn.commit()
-            self._conn.close()
-
 
 class storage(object):
     """本类存储到sql当中""" 
@@ -148,10 +86,64 @@ class storage(object):
             self._conn.close()
         return result
 
+class checkAlive(object):
+    '''本类专门用于检测代理的可用性
+       原理是使用代理访问baidu，看延迟
+       存储到专门的数据库，ipaddr,site,lag,statu,updatetime
+    '''
+    def __init__(self):
+        self._url = 'http://m.51job.com'
+        self._urlconfirm = '51job'
+        self._sqlname = 'proxy.db'
+        self._conn = sqlite3.connect(self._sqlname)
+        self._c = self._conn.cursor()
+        self._c.execute('CREATE TABLE IF NOT EXISTS status (ipaddr TEXT,site TEXT,lag TEXT,statu TEXT,updatetime TEXT)')
+        self._c.execute('SELECT ishttp,ipaddr,port FROM proxy')
+        self._httpipport = self._c.fetchall()
+        #将http ipaddr port 结果放在list当中，以便调用，这样不会重复读取数据库
+        self._c.execute('SELECT COUNT(ipaddr) FROM proxy')
+        self._proxynum = self._c.fetchall()[0][0]
+        #这里获取一共有多少条代理需要验证
+        
+    def isAlive(self):
+        num = 1
+        insertvalue = []
+        try:
+            for row in self._httpipport:
+                ishttp = row[0]
+                ipport = ('%s:%s' % (row[1],row[2]))
+                #循环读取数据库当中的ishttp和ipport字段
+                proxy_support = request.ProxyHandler({ishttp:ipport})
+                opener = request.build_opener(proxy_support)
+                request.install_opener(opener)
+                #读取对应网页用以测试速度
+                print('%s/%s 正在验证代理：%s:%s ...' % (num,self._proxynum,row[1],row[2]))
+                t1 = time.time()
+                with opener.open(self._url) as f:
+                    if self._urlconfirm in str(f.info()):
+                        sitestatu = 'OK'
+                    else: sitestatu = 'NOK'
+                    t2 = time.time()
+                lagtime =('%0.2f' % ((t2-t1)*1000))
+                insertvalue.append((row[1],self._url,lagtime,sitestatu,'2017'))
+                #这里将需要写入数据库的内容先放在list当中，方便后续一次性execuatemany写入
+                print('访问验证结果为 %s，延迟 %s ms' % (sitestatu,lagtime))
+                num = num + 1
+
+        finally:
+            self._c.executemany('INSERT INTO status VALUES (?,?,?,?,?)',insertvalue)
+            self._c.execute('SELECT COUNT(ipaddr) FROM status')
+            result = self._c.fetchall()[0][0]
+            print('合计验证了 %s 条结果' % result)
+            #查询写入多少条结果
+            self._c.close()
+            self._conn.commit()
+            self._conn.close()
+
 
 
 proxylist = getProxy()
-proxyresult = proxylist.decodePage()
+proxyresult = proxylist.getxici()
 print(proxyresult)
 storageproxy = storage(**proxyresult)
 print(storageproxy.storeToSQL())
