0f3bba5277c50202406d33363d284365a4157345
root <root@debian>
2017-05-10 23:17:18 +0800

    精简获取代理方式

--- proxyGetter.py
+++ proxyGetter.py
@@ -3,54 +3,37 @@ from urllib import request
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 from collections import OrderedDict
+import requests
 import re
 import time
 import sqlite3
+
 class getProxy(object):
     """docstring for getProxyPage"""
     """本类用于抓取代理服务器网页"""
     """包含多个代理服务器的网页"""
     def __init__(self):
-        super(getProxy, self).__init__()
-        #建立headers
+        #头headers
         self._xiciheaders = {'Host':'www.xicidaili.com','User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0.1 Waterfox/51.0.1'}
-        #全局变量，存储抓取页面，解析结果
-        self._proxyStorage = {}
+        #requests sessions
+        self._s = requests.Session()
 
+    @property
     def getxici(self):
-        #使用代理获取地址
-        #proxy_support = request.ProxyHandler({'http':'10.166.1.37:8080'})
-        #opener = request.build_opener(proxy_support)
-        #request.install_opener(opener)
         #读取对应网页
-        url = 'http://www.xicidaili.com/wn/'
-        req = request.Request(url,headers=self._xiciheaders)
-        with request.urlopen(req) as f:
-            page = f.read().decode('utf-8')
+        proxyStorage = {}
+        url = 'http://www.xicidaili.com/nn/'
+        r = self._s.get(url,headers = self._xiciheaders)
+        page = r.text
 
         soup = BeautifulSoup(page,'html.parser')
         soupFirst = soup.find_all('tr')
-        templist = []
+        l = map(lambda x:x.stripped_strings,soupFirst)
+        n = (m for m in [i for i in l])
+        print(n)
         #此处通过stripped_strings方法获取不带空格的，所有html标签的text，为一个generator
         #验证templist长度，确认抓取内容中不含地理位置，如不含有则手动添加，保证list长度统一
-        for i in range(1,len(soupFirst)):
-            for text in soupFirst[i].stripped_strings:
-                templist.append(text)
-            if len(templist) == 7:
-                pass
-            else:
-                templist.insert(2,'无地理位置')
-        #此处将list按照位置加入倒字典当中并返回
-            self._proxyStorage['%s:%s' % (templist[0],templist[1])] = (
-                                                                        templist[0],
-                                                                        templist[1],
-                                                                        templist[4],
-                                                                        templist[3],
-                                                                        templist[2]
-                                                                      )
-        #此处重置templist，否则每次添加内容到同一个templist当中，导致字典只能获取第一个代理的值
-            templist = []
-        return self._proxyStorage
+        #return proxyStorage
 
 class storage(object):
     """本类存储到sql当中""" 
@@ -141,12 +124,6 @@ class checkAlive(object):
             self._conn.close()
 
 
-
-proxylist = getProxy()
-proxyresult = proxylist.getxici()
-print(proxyresult)
-storageproxy = storage(**proxyresult)
-print(storageproxy.storeToSQL())
-checkresult = checkAlive()
-checkresult.isAlive()
-
+if __name__ == '__main__':
+    p = getProxy()
+    print(p.getxici)
